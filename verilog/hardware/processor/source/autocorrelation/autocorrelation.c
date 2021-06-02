8b5a71815a45aac70b2fbc108c0e078e9377a9ec
matthewejones <mej34@cam.ac.uk>
2021-06-02 12:54:45 +0000

    compiled autocorrelation

--- verilog/hardware/processor/source/autocorrelation/autocorrelation.c
+++ verilog/hardware/processor/source/autocorrelation/autocorrelation.c
@@ -1,21 +1,20 @@
 #include "autocorrelation_data.h"
 
-
 int main(void)
 {
 
-    float autocorrelation[50];
-
+    int autocorrelation[50];
+    
     for (int k = 0; k < data_len; k++)
     {
+        int sum = 0;
         //Find the sum over where they both have a value
-        float sum = 0;
         for (int n = 0; n < data_len - k; n++)
         {
             sum += data[n] * data[n+k];
         }
         autocorrelation[k] = sum/(data_len-k);
-	}	
+    }
 
     return 0;
 }
