d5a07691815a1ec54d886cdbe6cadf200ba5b87c
Andrew Salkeld <andrew@ajsalkeld.com>
2021-06-04 17:04:46 +0100

    Reduce size to less than 2KBits

--- verilog/hardware/processor/source/fft-looped/fft-looped.c
+++ verilog/hardware/processor/source/fft-looped/fft-looped.c
@@ -1,46 +1,65 @@
 //#include <stdio.h>
-#include <math.h>
-#include <complex.h>
- 
-double PI;
-typedef double complex cplx;
+#define PI 3
 
-void _fft(cplx buf[], cplx out[], int n, int step)
+int cos(int x) {
+	return 1-x*x/2 + x*x*x*x/24;
+}
+
+int sin(int x) {
+	return x - x*x*x/6 + x*x*x*x*x / 120;
+}
+
+void _fft(int buf_r[], int buf_i[], int out_r[], int out_i[], int n, int step)
 {
 	if (step < n) {
-		_fft(out, buf, n, step * 2);
-		_fft(out + step, buf + step, n, step * 2);
+	  _fft(out_r, out_i, buf_r, buf_i, n, step * 2);
+	  _fft(out_r + step, out_i + step, buf_r + step, buf_i + step, n, step * 2);
  
 		for (int i = 0; i < n; i += 2 * step) {
-			//cplx t = cexp(-I * PI * i / n) * out[i + step];
-			cplx t = (cos(-PI * i / n) + I * sin(-PI * i / n) ) * out[i + step];
-			buf[i / 2]     = out[i] + t;
-			buf[(i + n)/2] = out[i] - t;
+			int real = cos(-PI * i / n) * out_r[i + step];
+			int imag = sin(-PI * i / n) * out_i[i + step];
+
+			buf_r[i / 2]     = out_r[i] + real;
+			buf_r[(i + n)/2] = out_r[i] - real;
+
+			buf_i[i / 2]     = out_i[i] + imag;
+			buf_i[(i + n)/2] = out_i[i] - imag;
 		}
 	}
 }
- 
-void fft(cplx buf[], int n)
+
+void fft(int buf_r[], int buf_i[], int n)
 {
-	cplx out[n];
-	for (int i = 0; i < n; i++) out[i] = buf[i];
+	int out_i[n];
+	int out_r[n];
+	for (int i = 0; i < n; i++) out_r[i] = buf_r[i];
+	for (int i = 0; i < n; i++) out_i[i] = buf_i[i];
  
-	_fft(buf, out, n, 1);
+	_fft(buf_r, buf_i, out_r, out_i, n, 1);
 }
+/*
+void show(const char * s, int buf_r[], int buf_i[]) {
+	printf("%s", s);
+	for (int i = 0; i < 8; i++)
+		    printf("(%i, %i) ", buf_r[i], buf_i[i]);
+		    }*/
  
 int main()
 {
-	volatile unsigned int *debugPin = (unsigned int *)0x2000;
-	unsigned char output = 0xFF;
-	*debugPin = output;
+  //volatile unsigned int *debugPin = (unsigned int *)0x2000;
+  int placeholder;
+  int *debugPin = &placeholder;
+  unsigned char output = 0xFF;
+  *debugPin = output;
 
-	while (1) {
-		PI = atan2(1, 1) * 4;
-		cplx buf[] = {1, 1, 1, 1, 0, 0, 0, 0};
-	 
-		fft(buf, 8);
-		output = ~output;
-		*debugPin = output;
- 	}
-	return 0;
+  while (1) {
+    int buf_r[] = {1, 1, 1, 1, 0, 0, 0, 0};
+    int buf_i[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    fft(buf_r, buf_i, 8);
+    //show("\n\nFFT : ", buf_r, buf_i);
+    if (output==0xFF) output = 0x00;
+    else output = 0xFF;
+    *debugPin = output;
+    }
+  return 0;
 }
